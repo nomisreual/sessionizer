29d3f02fc7c94faf007ea24916c8c9c1a9e1e121
Simon Antonius Lauer <nomisreual@proton.me>
2025-10-29 15:28:28 +0100

    feat: move project root to env variable instead of hard coded in build step

--- default.nix
+++ default.nix
@@ -1,40 +1,46 @@
-{pkgs ? import <nixpkgs> {}}: let
-  PROJECTS =
-    if pkgs.stdenv.isLinux
-    then "/home/simon/Projects"
-    else "/Users/simon/Projects";
-in
-  pkgs.writeShellApplication {
-    name = "sessionizer";
-    runtimeInputs = with pkgs; [findutils fzf fd tmux];
-    text = ''
-      if [[ -v TMUX ]]; then
-        selected=$(fd . -t d -d 1 ${PROJECTS} | fzf --tmux)
+{pkgs ? import <nixpkgs> {}}:
+pkgs.writeShellApplication {
+  name = "sessionizer";
+  runtimeInputs = with pkgs; [findutils fzf fd tmux];
+  text = ''
+    # Check whether Project root is set. Fall back to HOME otherwise.
+    if [[ -v PROJECT_ROOT ]]; then
+      PROJECTS=$PROJECT_ROOT
+    else
+      PROJECTS=$HOME
+    fi
+
+    # Display selection differently based on run within a tmux session or not.
+    if [[ -v TMUX ]]; then
+      selected=$(fd . -t d -d 1 "$PROJECTS" | fzf --tmux)
+    else
+      selected=$(fd . -t d -d 1 "$PROJECTS" | fzf --height 40% --layout reverse --border)
+    fi
+
+    # Based on project selection create a sanitized name for tmux session.
+
+    # Full paths
+    clean_path=$(realpath "$selected")
+
+    # Select name for session
+    selected_name=$(basename "$selected")
+
+    # Sanitize session name
+    selected_name=''${selected_name//[^a-zA-Z0-9_.-]/_}
+
+    # Spin up or attach to existing tmux session.
+    if [[ -v TMUX ]]; then
+      if tmux has-session -t="$selected_name" 2>/dev/null; then
+        tmux switch-client -t "$selected_name"
+        clear
       else
-        selected=$(fd . -t d -d 1 ${PROJECTS} | fzf --height 40% --layout reverse --border)
-      fi
-
-      # full paths
-      clean_path=$(realpath "$selected")
-
-      # select name for session
-      selected_name=$(basename "$selected")
-
-      # sanitize session name
-      selected_name=''${selected_name//[^a-zA-Z0-9_.-]/_}
-
-      if [[ -v TMUX ]]; then
-        if tmux has-session -t="$selected_name" 2>/dev/null; then
-          tmux switch-client -t "$selected_name"
-          clear
-        else
-          tmux new-session -A -s "$selected_name" -c "$clean_path" -d
-          tmux switch-client -t "$selected_name"
-          clear
-        fi
-      else
-        tmux new-session -A -s "$selected_name" -c "$clean_path"
+        tmux new-session -A -s "$selected_name" -c "$clean_path" -d
+        tmux switch-client -t "$selected_name"
         clear
       fi
-    '';
-  }
+    else
+      tmux new-session -A -s "$selected_name" -c "$clean_path"
+      clear
+    fi
+  '';
+}
