61060921daedcd2fd1bea3b8c6687b7c661cc2a2
Simon Antonius Lauer <nomisreual@proton.me>
2025-10-31 12:01:42 +0100

    feat: provide flags. not implemented yet

--- src/sessionizer.sh
+++ src/sessionizer.sh
@@ -28,12 +28,21 @@ preview_helper() {
 # Make the helper available in sub-processes (sub-shell spawning fzf)
 export -f preview_helper
 
-#TODO: allow overriding PROJECT_ROOT with a command flag.
-while getopts 'h' opt; do
+while getopts 'hnp' opt; do
   case "$opt" in
   h)
     usage
     exit 0
+    ;;
+  n)
+    # TODO: add functionality to create new dir for new project
+    echo "Would you like to create a new directory?"
+    exit 1 # not implemented yet. exit
+    ;;
+  p)
+    #TODO: allow overriding PROJECT_ROOT with a command flag.
+    echo "wanna override PROJECT_ROOT?"
+    exit 1 # not implemented yet. exit
     ;;
   *)
     usage >&2
@@ -42,23 +51,40 @@ while getopts 'h' opt; do
   esac
 done
 
-# TODO: check whether PROJECTS is a directory.
-# Similarly, check whether HOME as fallback is a directory.
-# Exit 1 otherwise.
+# TEST: add unit tests
 
 # Check whether Project root is set. Fall back to HOME otherwise.
+# Exit 1 if neither is set.
 if [[ -v PROJECT_ROOT ]]; then
   PROJECTS=$PROJECT_ROOT
-else
+elif [[ -v HOME ]]; then
   PROJECTS=$HOME
+else
+  usage >&2
+  exit 1
 fi
 
+# TEST: add unit tests
+
+# Similarly, check whether HOME as fallback is a directory.
+# Exit 1 otherwise.
+if [[ ! -d "$PROJECTS" ]]; then
+  usage >&2
+  exit 1
+fi
+
+# TODO: enable adding multiple project dirs
+# add logic to create new dir if not exist (for n flag)
+# NOTE: PROJECTS_LIST returns a string with each project folder seperated by a new line
+PROJECTS_LIST=$(fd . -t d -d 1 "$PROJECTS")
+
 # Display selection differently based on run within a tmux session or not.
+# manually invoke bash with preview, to make sure exported helper works
+# TODO: add display flavour (maybe only display basename or highlight it)
 if [[ -v TMUX ]]; then
-  # manually invoke bash with preview, to make sure exported helper works
-  selected=$(fd . -t d -d 1 "$PROJECTS" | fzf --tmux --preview 'bash -c "preview_helper {}"')
+  selected=$(echo "$PROJECTS_LIST" | fzf --tmux --preview 'bash -c "preview_helper {}"')
 else
-  selected=$(fd . -t d -d 1 "$PROJECTS" | fzf --height 40% --layout reverse --border --preview 'bash -c "preview_helper {}"')
+  selected=$(echo "$PROJECTS_LIST" | fzf --height 40% --layout reverse --border --preview 'bash -c "preview_helper {}"')
 fi
 
 # Based on project selection create a sanitized name for tmux session.
